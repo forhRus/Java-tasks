6705982a199415f4979b296dbf14fe78d4ab8b6f
Sergey Shkaev <shkaev.ss@mail.ru>
2024-05-16 11:21:04 +0300

    Инициализация репозитория. Добавление первой задачи.

---
+++ 0. SE/src/level_1/ex1.java
@@ -0,0 +1,50 @@
+package level_1;
+
+import java.util.Scanner;
+
+/*
+1. Дано число. Проверьте, отрицательное оно или нет. Выведите об этом информацию в консоль.
+ */
+public class ex1 {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    while (true) {
+
+      System.out.print("Введите целое число: ");
+
+      if (scanner.hasNextInt()){
+        System.out.println(checkNumber(scanner.nextInt()));
+      } else {
+        break;
+      }
+    }
+
+    System.out.println("Выход из программы.");
+  }
+
+  public static String checkNumber(int number) {
+    StringBuilder res = new StringBuilder("Число %d - ".formatted(number));
+
+    if (isPositive(number)) {
+      res.append("положительное");
+    } else if (isNegative(number)) {
+      res.append("отрицательное");
+    } else {
+      res.append("ноль");
+    }
+
+    return res.toString();
+  }
+
+  public static boolean isNegative(int number) {
+    return number < 0;
+  }
+
+  public static boolean isPositive(int number) {
+    return number > 0;
+  }
+
+}
+
+
